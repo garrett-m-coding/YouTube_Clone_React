911abb19ee7328fbbcea4bc12f8033e0ce198a3c
JmA515 <janderson515@yahoo.com>
2021-10-19 17:39:14 -0700

    Paired programming with Garrett. Refactored DisplayCommenomments css file with new styling.

--- src/components/DisplayComments/DisplayComments.jsx
+++ src/components/DisplayComments/DisplayComments.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import './DisplayComments.css' 
+
 
 const DisplayComments = (props) => {
     console.log(props.comments);
     return (
         <div>
-                {console.log(props.videoSrc)}
-            <table>
+            <br/>
+            <h3>Comments</h3>    
+            {console.log(props.videoSrc)}
+            <table className="table">
                 <thead>
                     <tr>
-                        <th> Comments </th>
                     </tr>
                 </thead>
                 <tbody>
                     {props.comments.map((comment) => {
                         return (
                             <tr key={comment.id}>
-                                <td>{comment.body}</td>
-                                <td><button href = "#" className = "btn btn-outline-success button-row" onClick={() => props} >Like</button></td>
-                                <td><button href = "#" className = "btn btn-outline-danger button-row" onClick={() => props} >Dislike</button></td>
+                                <td className="td">{comment.body}</td>
+                                <td className="buttonCenter"><button href = "#" className = "btn btn-outline-success button-row" onClick={() => props} >Like</button></td>
+                                <td className="buttonCenter"><button href = "#" className = "btn btn-outline-danger button-row" onClick={() => props} >Dislike</button></td>
                             </tr>
                         );
                     })}
