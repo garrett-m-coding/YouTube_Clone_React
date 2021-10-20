6a042a02efab5dbed9ea855a75f3f08c150511dd
JmA515 <janderson515@yahoo.com>
2021-10-19 17:47:04 -0700

    Paired programming with Garrett. Refactored VideoInfo styl styling css file.

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -13,11 +13,10 @@ const VideoInfo = (props) => {
         <div>
             <VideoPlayer videoSrc = {videoSrc} />
             <br/>
-            <h3 class="rounded">{props.video.snippet.title}</h3>
-            <br/>
+            <h3 className="title">{props.video.snippet.title}</h3>
             <br/>
             {console.log(props.comments)}
-            <p class="rounded">{props.video.snippet.description}</p>
+            <p className="title">{props.video.snippet.description}</p>
             <AddComment 
               addComment={props.addComment} 
               videoSrc = {videoSrc}/>
